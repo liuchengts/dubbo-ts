1c3be6e02f4f53ddf2868bbe0af8444bd2f14145
刘成 <790809568@qq.com>
2017-12-07 11:19:47 +0800

    init

--- dubbo-test/dubbo-test-consumer/src/main/java/com/example/demo/DemoController.java
+++ dubbo-test/dubbo-test-consumer/src/main/java/com/example/demo/DemoController.java
@@ -5,16 +5,20 @@ import dubbo.test.DemoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 /**
  * Created by apple on 2017/12/6.
  */
 @RestController
 public class DemoController {
+    static Logger logger = Logger.getLogger(DemoController.class.getName());
     @DubboConsumer
     private DemoService demoService;
 
     @RequestMapping
-    public String index(){
+    public String index() {
+        logger.info("准备调用in()");
         return demoService.in();
     }
 }
--- dubbo-test/dubbo-test-provider/src/main/java/dubbo/test/provider/DemoServiceImpl.java
+++ dubbo-test/dubbo-test-provider/src/main/java/dubbo/test/provider/DemoServiceImpl.java
@@ -4,14 +4,18 @@ import com.alibaba.dubbo.config.annotation.Service;
 import dubbo.test.DemoService;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 /**
  * Created by apple on 2017/12/6.
  */
 @Service
 @Component
 public class DemoServiceImpl implements DemoService {
+  static Logger logger=Logger.getLogger(DemoServiceImpl.class.getName());
     @Override
     public String in() {
+        logger.info("in()响应调用");
         return "index";
     }
 }
